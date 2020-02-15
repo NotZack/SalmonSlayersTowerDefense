0e9e84fb3169d4628ffa88da25db4712de6387fe
NotZack <zackarynichol@gmail.com>
2020-02-14 22:41:43 -0600

    Added basic stream map and stream coord tracking

---
+++ Assets/Scripts/Coordinators/StreamCoordinator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class StreamCoordinator : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tilemap tilemap = GetComponent<Tilemap>();
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+        List<Vector2> streamLocations = new List<Vector2>();
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null)
+                {
+                    streamLocations.Add(new Vector2(x, y));
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
