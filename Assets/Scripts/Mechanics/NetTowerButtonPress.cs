40df1ce0c73d186df84949555a1e82e8e6dc54cb
NotZack <zackarynichol@gmail.com>
2020-02-15 01:35:16 -0600

    Added tower placement functionality

--- Assets/Scripts/Mechanics/NetTowerButtonPress.cs
+++ Assets/Scripts/Mechanics/NetTowerButtonPress.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaceTower : MonoBehaviour
+public class NetTowerButtonPress : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class PlaceTower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameCoordinator.createNetTower();
     }
 }
